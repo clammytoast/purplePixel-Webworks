caecfc6d80c01043fe3ce4a94a4c43b07593a067
clammytoast <151802002+clammytoast@users.noreply.github.com>
2024-02-15 12:28:07 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-
+// Initialize Typed.js for typing animation
+var typed = new Typed('#typing-element', {
+    strings: ['Welcome to PurplePixel Webworks!', 'where creativity meets innovation.'],
+    typeSpeed: 50,
+    loop: true // optional: whether the typing should loop
+});
